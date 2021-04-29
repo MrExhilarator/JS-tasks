4e7b038bfceb94796406edcd2c7462b336af3bec
Mr Exhilarator <81799061+MrExhilarator@users.noreply.github.com>
2021-04-29 10:33:27 +0530

    Update rentalCarCost.js

--- rentalCarCost.js
+++ rentalCarCost.js
@@ -1,6 +1,8 @@
-function rentalCarCost(d) {
-    if(d<3)return d*40;
-    else if(d<7)return d*40-20;
-    else return d*40-50;
+/* rentalCarCost returns the cost to rent a 
+car based on how many days one wants the car for rent*/
+function rentalCarCost(noOfDays) {
+    if (noOfDays < 3) return noOfDays * 40;
+    else if (noOfDays < 7) return noOfDays * 40 - 20;
+    else return noOfDays * 40 - 50;
 }
-  
+ 
