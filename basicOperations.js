7bee0fe59a8a37375a8a1f5e73c67849c17ddd62
Mr Exhilarator <81799061+MrExhilarator@users.noreply.github.com>
2021-04-29 10:32:23 +0530

    Update basicOperations.js

--- basicOperations.js
+++ basicOperations.js
@@ -1,17 +1,19 @@
-function basicOp(operation, value1, value2)
-{
-  switch(operation){
-      case "+":
-      return value1+value2;
+/*basicOp returns the value of arithmetic operation
+performed between given two values based on the given operator*/
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
       break;
-      case "-":
-      return value1-value2;
+    case "-":
+      return value1 - value2;
       break;
-      case "*":
-      return value1*value2;
+    case "*":
+      return value1 * value2;
       break;
-      case "/":
-      return value1/value2;
+    case "/":
+      return value1 / value2;
       break;
   }
 }
+
