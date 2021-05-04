08094b628887e04542f4d72828a632f0c7b00877
Vikas Myadam <Vikas_Myadam@epam.com>
2021-05-04 19:15:46 +0530

    implemented es6 syntax

--- TDDconstruct.js
+++ TDDconstruct.js
@@ -1,8 +1,7 @@
 /*construct returns an object 
 which has name, values as properties*/
-function construct(Class) {
-  let object = new Class;
-  object.name = 'John';
-  object.values = [1, 2, 3, 4, 5];
+function construct(Class, ...args) {
+  let object = new Class();
+  Class.apply(object, args);
   return object;
 }
